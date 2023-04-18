67715a4f5da15af8b2ce8d9dd19cdafc6679aa16
unknown <freekodelka86@gmail.com>
2023-04-18 19:47:18 +0300

    Решены задачи 25 и 27

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿/*Напишите цикл, который принимает на вход два числа 
+(A и B) и возводит число A в натуральную степень B.*/
+int a,b;
+void EnterNumber() 
+{
+    Console.WriteLine("Enter number a: ");
+    a = int.Parse (Console.ReadLine());
+    Console.WriteLine("Enter number b: ");
+    b = int.Parse (Console.ReadLine());    
+}
+
+bool isNatural(int number) 
+{
+    if (number < 0)
+    {
+        Console.WriteLine("Введите положительное число!");
+        return false;
+    }
+    return true;
+}
+
+int NaturalPow(int a, int b)
+{
+    return (int)Math.Pow(a,b);
+}
+
+EnterNumber();
+if (isNatural(b))
+    Console.WriteLine(NaturalPow(a, b));
+
---
+++ Task27/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.*/
+
+/*int Input (int number);
+{
+    Console.WriteLine("Enter number: ");
+    int number = int.Parse(Console.ReadLine());
+}*/
+
+int SumDigital (int number)
+{
+    int result = 0;
+  
+    for (int i = 0; i <= number; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}
+
+Console.WriteLine("Enter number: ");
+Console.WriteLine(SumDigital(int.Parse(Console.ReadLine())));
